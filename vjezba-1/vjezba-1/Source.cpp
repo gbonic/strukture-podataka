3fe3a77aad550996f9d0c0f981c52c33351a30b9
gbonic <gbonic00@fesb.hr>
2023-10-09 20:50:03 +0200

    Added a while loop

--- vjezba-1/vjezba-1/Source.cpp
+++ vjezba-1/vjezba-1/Source.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main() {
-	cout << "Hello World!";
+	int a;
+	cout << "Upisi broj izmedu 4 i 9: ";
+	cin >> a;
+	while (a < 4 | a>9) {
+		printf("Krivo!");
+		cout << "Upisi broj izmedu 4 i 9: ";
+		cin >> a;
+	}
+	printf("Broj je u ispravnom intervalu.");
 	return 0;
 }
