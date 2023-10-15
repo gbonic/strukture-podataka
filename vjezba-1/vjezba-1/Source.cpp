eae924a6fcc00f8273542b4258e96d844f1f7428
gbonic <gbonic00@fesb.hr>
2023-10-15 19:27:05 +0200

    Added a function

--- vjezba-1/vjezba-1/Source.cpp
+++ vjezba-1/vjezba-1/Source.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
+void funkcija() {
+	cout << "\nHvala na pokusaju!\n";
+}
+
 int main() {
 	int a;
 	cout << "Upisi broj izmedu 4 i 9: ";
 	cin >> a;
 	while (a < 4 | a>9) {
-		printf("Krivo!");
+		printf("Krivo!\n");
 		cout << "Upisi broj izmedu 4 i 9: ";
 		cin >> a;
 	}
 	printf("Broj je u ispravnom intervalu.");
+
+	funkcija();
 	return 0;
 }
